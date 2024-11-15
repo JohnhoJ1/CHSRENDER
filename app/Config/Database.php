899ea774815d3c25bdb4a7e9c44bc55f879610a0
JohnhoJ1 <johnyonjan1234@gmail.com>
2024-11-15 22:31:23 +0000

    Update base URL to Render app link

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,31 +24,25 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-    public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'sql103.infinityfree.com',
-        'username'     => 'if0_37706180',
-        'password'     => 'TunedInFlat@202',
-        'database'     => 'if0_37706180_cultural_heritage',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => (ENVIRONMENT !== 'production'),
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
+    public $default = [
+        'hostname' => getenv('DB_HOST'),
+        'username' => getenv('DB_USERNAME'),
+        'password' => getenv('DB_PASSWORD'),
+        'database' => getenv('DB_NAME'),
+        'DBDriver' => getenv('DB_DRIVER'),
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug' => (ENVIRONMENT !== 'production'),
+        'charset' => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre' => '',
+        'encrypt' => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port' => getenv('DB_PORT'),
     ];
+    
 
     //    /**
     //     * Sample database connection for SQLite3.
